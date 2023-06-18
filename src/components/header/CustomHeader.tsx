1b6263e5e6633b8d20f4009e73c1ac45560c8b0b
tahapek5454 <tahapek5454@gmail.com>
2023-06-18 22:21:21 +0300

    feat: add new carousel

--- src/components/header/CustomHeader.tsx
+++ src/components/header/CustomHeader.tsx
@@ -1,34 +1,77 @@
 import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from 'react-bootstrap/Carousel';
 // import "./CustomNavbar.css"
+import "./CustomHeader.css"
 import imgHeader from './header.jpg';
+import imgHeader2 from './header2.jpg';
+import imgHeader3 from './header3.jpg';
 
 function CustomHeader() {
-      return  (
-    <div id="carouselExampleDark" className="carousel carousel-white slide" data-bs-ride="carousel">
-    <div className="carousel-indicators">
-      {/* <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button> */}
-      
-    </div>
-      <div >
-        <img  src={imgHeader} alt={"alt"}  style={{width: "100%", height:"auto"}}   />
-        <div className="carousel-caption d-none d-md-block " >
-          <h5>Harika Coffee</h5>
-          <p>blah bla bla bla</p>
-       
-        </div>
 
-    </div>
-    {/* <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span className="visually-hidden">Previous</span>
-    </button>
-    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-      <span className="carousel-control-next-icon" aria-hidden="true"></span>
-      <span className="visually-hidden">Next</span>
-    </button> */}
-  </div>
+  return(
+    <Carousel>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src={imgHeader}
+        alt="First slide"
+      />
+      <Carousel.Caption>
+        <h3>First slide label</h3>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src={imgHeader}
+        alt="Second slide"
+        
+      />
+
+      <Carousel.Caption>
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src={imgHeader}
+        alt="Third slide"
+      />
+
+      <Carousel.Caption>
+        <h3>Third slide label</h3>
+        <p>
+          Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+        </p>
+      </Carousel.Caption>
+    </Carousel.Item>
+  </Carousel>
   )
+     
 }
 
 export default CustomHeader
+
+
+// return  (
+//   <div id="carouselExampleDark" className="carousel carousel-white slide" data-bs-ride="carousel">
+//   <div className="carousel-indicators"> 
+//   </div>
+//     <div >
+//       <img  src={imgHeader} alt={"alt"}  style={{width: "100%", height:"auto"}}   />
+//       <div className="carousel-caption d-none d-md-block " >
+//         <h5>Harika Coffee</h5>
+//         <p>blah bla bla bla</p>
+     
+//       </div>
+
+//   </div>
+
+// </div>
+// )
