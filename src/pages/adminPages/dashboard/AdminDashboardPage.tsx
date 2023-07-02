49dc52c607ecfb1f8eab4cc1e3373c6749254042
tahapek5454 <tahapek5454@gmail.com>
2023-07-02 13:43:37 +0300

    feat: add css for dashbıard

--- src/pages/adminPages/dashboard/AdminDashboardPage.tsx
+++ src/pages/adminPages/dashboard/AdminDashboardPage.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Route, Routes, useNavigate } from 'react-router-dom'
 import AdminOrdersPage from '../orders/AdminOrdersPage'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ListGroup from 'react-bootstrap/ListGroup';
 import AdminGraphicPage from '../graphic/AdminGraphicPage';
+import './AdminDashboardPage.css'
 function AdminDashboardPage() {
     const navigate = useNavigate()
+    const [activeIndex, setActiveIndex] = useState<number>(0);
 
-
-    const goToClickedArea = (area:string) => {
+    const goToClickedArea = (area:string, key:number) => {
+        setActiveIndex(key)
         navigate(area)
     }
 
@@ -18,21 +20,18 @@ function AdminDashboardPage() {
 
             <div className="row">
                 <div className="col-lg-2 col-md-2 col-sm-12 col-12">
-                    <ListGroup defaultActiveKey="#link1">
-                        <ListGroup.Item action onClick={()=> goToClickedArea("*")} >
+                    <ListGroup>
+                        <ListGroup.Item  action onClick={()=> goToClickedArea("*", 0)} className={activeIndex === 0 ? 'activeColor' : "" } >
                             Gösterge Paneli
                         </ListGroup.Item>
-                        <ListGroup.Item action onClick={()=> goToClickedArea("orders")} >
+                        <ListGroup.Item  action onClick={()=> goToClickedArea("orders", 1)} className={activeIndex === 1 ? 'activeColor' : "" } >
                             Siparişler
                         </ListGroup.Item>
-                        <ListGroup.Item action disabled>
+                        <ListGroup.Item  action disabled>
                             Ürünler
                         </ListGroup.Item>
-                        <ListGroup.Item action disabled>
+                        <ListGroup.Item  action disabled>
                             Kategoriler
-                        </ListGroup.Item>
-                        <ListGroup.Item action disabled>
-                            Özel Menüler
                         </ListGroup.Item>
                     </ListGroup>
                 </div>
